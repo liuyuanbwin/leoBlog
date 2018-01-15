dc9c97be04ce7e8a8f1626567301ad065d5898d1
LeoLau <liuyuansix@163.com>
2018-01-15 16:30:40 +0800

    删掉所有从5.1开始

---
+++ application/admin/controller/Second.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac1
+ * Date: 2018/1/15
+ * Time: 下午3:21
+ */
+namespace app\admin\controller;
+class Secord
+{
+    public function main()
+    {
+        return "secord Main Method";
+    }
+}
